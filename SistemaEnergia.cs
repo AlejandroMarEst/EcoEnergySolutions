ea25dcc65d69e85a1e1ed2d29955a26ef2005ab4
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-01-20 11:54:08 +0100

    Created classes and class diagram, configured sonarQube, closes #1 #2

---
+++ SistemaEnergia.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EcoEnergySolutions
+{
+    public abstract class SistemaEnergia
+    {
+
+    }
+}
---
+++ SistemaSolar.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EcoEnergySolutions
+{
+    public class SistemaSolar : SistemaEnergia
+    {
+
+    }
+}
