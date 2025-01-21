c4a8e826552ba095e9cb53a0e0792eb6f31430a2
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-01-21 13:10:49 +0100

    Finished Methods and properties Closes #6

--- SistemaEolic.cs
+++ SistemaEolic.cs
@@ -6,10 +6,21 @@ namespace EcoEnergySolutions
     {
         public double WindVelocity {  get; set; }
         public SistemaEolic(double windVelocity)
-        { 
-            WindVelocity = windVelocity;
-            Energy = CalculEnergia(WindVelocity);
+        {
+            if (CheckParameter(windVelocity))
+            {
+                Type = "Wind energy";
+                WindVelocity = windVelocity;
+                Energy = CalculEnergia(WindVelocity);
+                Date = DateTime.Now;
+                SimulationNumber++;
+            }
         }
-        public double CalculEnergia(double WindVelocity) => Math.Pow(WindVelocity, 3) * 0.2;
+        public double CalculEnergia(double WindVelocity) => Math.Round(Math.Pow(WindVelocity, 3) * 0.2, 2);
+        public bool CheckParameter(double WindVelocity) => WindVelocity >= 20;
+        public void ShowSimulation()
+        {
+            Console.WriteLine($"|\tSimulation Number\t|\tType\t\t|\tEnergy Produced\t\t|\t\tDate\t\t|\n|\t\t{SimulationNumber}\t\t|\t{Type}\t|\t\t{Energy}\t\t|\t{Date}\t|");
+        }
     }
 }
--- SistemaHidroelectric.cs
+++ SistemaHidroelectric.cs
@@ -7,9 +7,21 @@ namespace EcoEnergySolutions
         public double WaterLevel { get; set; }
         public SistemaHidroelectric(double waterLevel)
         {
-            WaterLevel = waterLevel;
-            Energy = CalculEnergia(WaterLevel);
+            if (CheckParameter(waterLevel))
+            {
+                Type = "Hydroelectric energy";
+                WaterLevel = waterLevel;
+                Energy = CalculEnergia(WaterLevel);
+                Date = DateTime.Now;
+                SimulationNumber++;
+            }
         }
-        public double CalculEnergia(double WaterLevel) => WaterLevel * 9.8 * 0.8;
+        public double CalculEnergia(double WaterLevel) => Math.Round(WaterLevel * 9.8 * 0.8, 2);
+        public bool CheckParameter(double WaterLevel) => WaterLevel >= 5;
+        public void ShowSimulation()
+        {
+            Console.WriteLine($"|\tSimulation Number\t|\t\tType\t\t|\tEnergy Produced\t|\t\tDate\t\t\t\t|\n|\t\t{SimulationNumber}\t\t|\t{Type}\t|\t\t{Energy}\t|\t{Date}\t|");
+        }
+
     }
 }
